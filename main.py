72933dfdb96a9f4bd6aeb7ceefd5c8eba1c016f7
Bradley Gatewood <gatewoodbt@outlook.com>
2020-10-11 00:04:29 -0400

    add title bar and task labels

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import tkinter as tk
+import tkinter.font
+import datetime
+
+
+def get_daily_log_topic():
+    today = datetime.date.today()
+    topic = str(today.month) + '.' + str(today.day) + '.'
+    weekdays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    topic += weekdays[today.weekday()]
+    return topic
 
 
 class App(tk.Frame):
@@ -6,19 +16,22 @@ class App(tk.Frame):
         super().__init__(root)
         root.title('Notebook')
         root.geometry('360x640')
-        self.create_widgets()
         self.pack(expand=tk.YES, fill=tk.BOTH, padx=8, pady=8)
+        self.create_widgets()  # TODO: remove from constructor
     
     def create_widgets(self):
+        # change the default font size
+        tk.font.nametofont('TkDefaultFont').configure(size=10)
+
         # build the task entry form
         form = tk.Frame(self)
         form.pack(side=tk.BOTTOM, fill=tk.BOTH)
 
         row = tk.Frame(form)  # top row
         row.pack(fill=tk.X, pady=4)
-        label = tk.Label(row, text='New task:', width=8)
+        label = tk.Label(row, text='New task:', width=10)
         label.pack(side=tk.LEFT)
-        self.entry = tk.Entry(row)
+        self.entry = tk.Entry(row, font=(None, 10))
         self.entry.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
 
         row = tk.Frame(form)  # bottom row
@@ -30,8 +43,27 @@ class App(tk.Frame):
         self.entry.bind('<Return>', lambda event: self.add_task())
         self.entry.focus()
 
+        # build the title bar (collection.topic)
+        frame = tk.Frame(self)
+        frame.pack(side=tk.TOP, fill=tk.X)
+        collection = 'Daily Log'
+        label = tk.Label(self, text=collection)
+        label.pack()
+        topic = get_daily_log_topic()
+        label = tk.Label(self, text=topic, font=(None, 12, 'bold'))
+        label.pack()
+
     def add_task(self):
-        print(self.entry.get())
+        # fetch the new task and clear the entry widget
+        bullet = '\u2022'
+        task = bullet + ' ' + str(self.entry.get())
+        self.entry.delete(0, tk.END)
+
+        # add the new task to the current collection
+        row = tk.Frame(self)
+        row.pack(fill=tk.X, padx=16)
+        label = tk.Label(row, text=task)
+        label.pack(side=tk.LEFT)
 
 
 if __name__ == '__main__':
